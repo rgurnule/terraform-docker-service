405037882671e5a6c15140167eb2235f3f077a19
Rahul Gurnule <srahulgurnule@gmail.com>
2019-12-11 13:28:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,15 @@ resource "docker_service" "mysql-service" {
     container_spec {
       image = var.mysql_image
 
-      env {
+      env = {
         MYSQL_ROOT_PASSWORD = var.mysql_root_password
       }
 
-      mounts = [
-        {
-          target = "/var/lib/mysql"
-          source = docker_volume.mysql_data_volume.name
-          type   = "volume"
-        }
-      ]
+      mounts  {
+                  target = "/var/lib/mysql"
+                  source = docker_volume.mysql_data_volume.name
+                  type   = "volume"
+      }
     }
     networks = [docker_network.private_bridge_network.name]
   }
@@ -28,7 +26,7 @@ resource "docker_service" "ghost-service" {
     container_spec {
       image = var.ghost_image
 
-      env {
+      env = {
          database__client               = "mysql"
          database__connection__host     = var.mysql_network_alias
          database__connection__user     = var.ghost_db_username
